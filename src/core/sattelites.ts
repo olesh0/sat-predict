62386c40ddec00d8fd95772de105bd1429d515e0
olesh <olegkirashchuk@gmail.com>
2021-02-13 10:50:08 +0200

    add full cache logic

--- src/core/sattelites.ts
+++ src/core/sattelites.ts
@@ -11,6 +11,7 @@ import { shortEnglishHumanizer } from "./utils"
 import celestrak from "../data/celestrack.json"
 
 const LOADED_FILES_PATH = "../data/cache"
+const TIME_FORMAT = "DD/MM/yyyy HH:mm:ss"
 
 interface OrbitInfo {
   satName: string;
@@ -72,7 +73,6 @@ export const predictPasses = ({
   const passEnd = end ||  Date.now() + 86400000
 
   const transits = jspredict.transits(tle, qth, passStart, passEnd, minimumElevation)
-  const TIME_FORMAT = "DD/MM/yyyy HH:mm:ss"
 
   const formatTime = (timestamp) => ({
     timestamp,
@@ -112,68 +112,89 @@ export const getSatInfo = ({ sattelite }) => {
 
 export const getSatsCategories = () => celestrak.map(({ section }) => section)
 
+interface UpdateCacheProps {
+  cachePath: string;
+  url: string;
+}
+
+const updateCache = async ({ cachePath, url }: UpdateCacheProps) => {
+  const { data } = await axios.get(url, { timeout: 10000 })
+
+  try {
+    fs.writeFileSync(cachePath, JSON.stringify({
+      created: Date.now(),
+      data: base64.encode(data),
+    }, null, 2), { flag: "w+" })
+
+    return Promise.resolve()
+  } catch (e) {
+    return Promise.reject(e)
+  }
+}
+
 export const fetchFullData = async () => {
   const failed = []
   const success = []
+  let fetched = 0
 
-  const promises = celestrak.map(({ section, url }, index) => {
-    return new Promise(async () => {
-      try {
-        const updateCache = async (cachePath: string) => {
-          const { data } = await axios.get(url, { timeout: 10000 })
+  const timeStart = Date.now()
 
-          try {
-            fs.writeFileSync(cachePath, JSON.stringify({
-              created: Date.now(),
-              data: base64.encode(data),
-            }, null, 2), { flag: "w+" })
+  const cachePromises = celestrak.map(async ({ section, url }) => {
+    try {
+      const cacheFilename = `${base64.encode(section)}.cache`
+      const cacheFilePath = path.join(__dirname, LOADED_FILES_PATH, cacheFilename)
+      const cacheExists = fs.existsSync(cacheFilePath)
 
-            return Promise.resolve()
-          } catch (e) {
-            return Promise.reject(e)
-          }
+      if (!cacheExists) {
+        await updateCache({ cachePath: cacheFilePath, url })
+        fetched += 1
+
+        console.log(`Created cache for ${cacheFilename}`)
+      } else {
+        const cacheBuffer = fs.readFileSync(cacheFilePath)
+        const cacheValues = Buffer.from(cacheBuffer).toString()
+
+        const parsedCache = JSON.parse(cacheValues)
+        const { created } = parsedCache
+
+        const cacheIsOutdated = moment(created).diff(Date.now() - (3600 * 6 * 1000)) < 0
+
+        if (cacheIsOutdated) {
+          console.log(`Updated outdated cache ${cacheFilename}`)
+
+          fetched += 1
+          updateCache({ cachePath: cacheFilePath, url })
         }
-
-        const cacheFilename = `${base64.encode(section)}.cache`
-        const cacheFilePath = path.join(__dirname, LOADED_FILES_PATH, cacheFilename)
-        const cacheExists = fs.existsSync(cacheFilePath)
-
-        if (!cacheExists) {
-          await updateCache(cacheFilePath)
-          console.log(`Updated cache for ${cacheFilename}`)
-        } else {
-          const cacheBuffer = fs.readFileSync(cacheFilePath)
-          const cacheValues = Buffer.from(cacheBuffer).toString()
-
-          if (index === 0) {
-            const parsedCache = JSON.parse(cacheValues)
-            const { data, created } = parsedCache
-
-            console.log("first value:")
-            console.log({
-              section,
-              data: `length is ${data.length}`,
-              created,
-            })
-          }
-
-          // console.log(`Reading cache values... ${cacheValues && cacheValues.length}`)
-        }
-
-        success.push({ section, url })
-      } catch (error) {
-        console.log(error)
-
-        failed.push({ section, url, error })
-      } finally {
-        return Promise.resolve()
       }
-    })
+
+      success.push({ section, url })
+    } catch (error) {
+      console.log(error)
+
+      failed.push({ section, url, error })
+    } finally {
+      return Promise.resolve()
+    }
   })
 
-  console.log("failed: ", failed)
+  await Promise.all(cachePromises)
 
-  await Promise.all(promises)
+  const timeEnd = Date.now()
+  const cacheGatherTime = (timeEnd - timeStart) / 1000
+  const cacheFileInfoPath = path.join(__dirname, "../data/cache.json")
 
-  return Promise.resolve('done')
+  fs.writeFileSync(cacheFileInfoPath, JSON.stringify({
+    lastUpdated: Date.now(),
+    failed: failed.length,
+    success: success.length,
+    cacheGatherTime,
+    itemsFetched: fetched,
+  }), { flag: "w" })
+
+  return Promise.resolve({
+    failed,
+    success,
+    cacheGatherTime,
+    itemsFetched: fetched,
+  })
 }
